c0c4da691b24f8a1f91e417c8a9ed74e33943f4f
Yaroslav Pyrikov <yarko92@rambler.ru>
2024-06-05 23:07:04 +0300

    Все же поменяю text на varchar и где забыл схему, то добавлю (хотя в DBeaver и так работает, если схему явно не указывать)

--- ScriptsSql/Script1.sql
+++ ScriptsSql/Script1.sql
@@ -4,13 +4,12 @@ set schema 'test';
 
 create table test.persons
 (
-    firstname      text,
-    surname        text,
+    firstname      varchar(255),
+    surname        varchar(255),
     age            smallint,
-    phone_number   text,
-    city_of_living text not null,
+    phone_number   varchar(50),
+    city_of_living varchar(255) not null,
     primary key (firstname, surname, age)
-
 );
 
 insert
@@ -18,23 +17,23 @@ into test.persons (firstname, surname, age, phone_number, city_of_living)
 values ('Yaroslav', 'Pyrikov', 31, '', 'Moscow');
 
 insert
-into persons (firstname, surname, age, phone_number, city_of_living)
+into test.persons (firstname, surname, age, phone_number, city_of_living)
 values ('Vasiliy', 'Ogurсhov', 31, '', 'Kazan');
 
 insert
-into persons (firstname, surname, age, phone_number, city_of_living)
+into test.persons (firstname, surname, age, phone_number, city_of_living)
 values ('Roberto', 'Carlos', 40, '', 'Brazil');
 
 insert
-into persons (firstname, surname, age, phone_number, city_of_living)
+into test.persons (firstname, surname, age, phone_number, city_of_living)
 values ('Vania', 'Poddupniy', 1, '', 'Volgograd');
 
 
 select firstname, surname
-from persons p
+from test.persons p
 where city_of_living = 'Moscow';
 
-select firstname, surname
-from persons p
+select *
+from test.persons p
 where age > 27
 order by age desc;
